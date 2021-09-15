95412e18747a3124e672c26fd701121064337f94
delaluz12 <75863526+delaluz12@users.noreply.github.com>
2021-09-15 06:49:30 -0500

    finished query.js

--- database/query.js
+++ database/query.js
@@ -23,39 +23,21 @@ class Query {
         return this.connection.query('SELECT employee.id, employee.first_name, employee.last_name, roles.title, dept.name AS department, roles.salary, CONCAT(manager.first_name, " ", manager.last_name) AS Manager FROM employee LEFT JOIN roles ON employee.roles_id = roles.id LEFT JOIN dept ON roles.dept_id = dept.id LEFT JOIN employee manager ON employee.manager_id = manager.id;')
     }
     //add dept pass in name only
-    addDept(newDept) {
-        return this.connection.query(`INSERT INTO dept (name) VALUES (${newDept});`, (err, result) => {
-            if (err) {
-              console.log(err);
-            }
-            console.log(result);
-          });
+    newDept(newDept) {
+        return this.connection.query(`INSERT INTO dept (name) VALUES (${newDept});`);
     }
     //add role pass in title, salary and dept # 
-    addRole(title, salary, dept) {
-        return this.connection.query(`INSERT INTO roles (title, salary, dept_id) VALUES (${title}, ${salary}, ${dept});`, (err, result) => {
-            if (err) {
-              console.log(err);
-            }
-            console.log(result);
-          });
+    newRole(title, salary, dept) {
+        return this.connection.query(`INSERT INTO roles (title, salary, dept_id) VALUES (${title}, ${salary}, ${dept});`);
     }
     //add employee pass in first, last, role & manager id #
-    addEmployee(first, last, roleId, managerId){
-        return this.connection.query(`INSERT INTO employee (first_name, last_name, roles_id, manager_id) VALUES (${first}, ${last}, ${roleId}, ${managerId});`, (err, result) => {
-            if(err){
-                console.log(err)
-            }
-            console.log(result)
-        });
+    newEmployee(first, last, roleId, managerId){
+        return this.connection.query(`INSERT INTO employee (first_name, last_name, roles_id, manager_id) VALUES (${first}, ${last}, ${roleId}, ${managerId});`);
     }
     //update employee role pass in employeeId, newRoleId
     updateEmployeeRole(employeeId, newRoleId){
-        return this.connection.query(`UPDATE employee SET roles_id = ${newRoleId} WHERE id = ${employeeId};`, (err, result) => {
-            if (err){
-                console.log(err)
-            }
-            console.log(result)
-        });
+        return this.connection.query(`UPDATE employee SET roles_id = ${newRoleId} WHERE id = ${employeeId};`);
     }
 }
+
+module.exports = new Query(connection);
--- index.js
+++ index.js
@@ -1,4 +1,110 @@
-const inquirer = require('inquirer');
-const database = require('./database');
+//object deconstruction to not have to type inquirer.prompt
+const { prompt } = require('inquirer');
+const query = require('./mydatabase/query');
 const cTable = require('console.table');
 
+
+function init() {
+    console.log('Welcome to workForce CMS');
+    //init inquirer prompts to grab choice
+    renderPrompts();
+}
+
+init();
+
+
+async function renderPrompts() {
+    const { choice } = await prompt([
+        {
+            type: 'list',
+            name: 'choice',
+            message: 'What would you lile to do?',
+            choices: [
+                {
+                    name: 'View All Departments',
+                    value: 'VIEW_DEPT'
+                },
+                {
+                    name: 'Add Department',
+                    value: 'ADD_DEPT'
+                },
+                {
+                    name: 'View All Roles',
+                    value: 'VIEW_ROLES'
+                },
+                {
+                    name: 'Add Role',
+                    value: 'ADD_ROLE'
+                },
+                {
+                    name: 'View All Employees',
+                    value: 'VIEW_EMPLOYEES'
+                },
+                {
+                    name: 'Update Employee Role',
+                    value: 'UPDATE_EMPLOYEE_ROLE'
+                },
+                {
+                    name: 'Quit',
+                    value: 'QUIT'
+                }
+            ]
+
+        }
+    ]);
+    console.log(choice);
+
+    //switch statement - use choice VALUE
+    switch (choice) {
+        case 'VIEW_DEPT':
+            viewAllDept();
+            break;
+        case 'ADD_DEPT':
+            addDept();
+            break;
+        case 'VIEW_ROLES':
+            viewAllRoles();
+            break;
+        case 'ADD_ROLE':
+            addRole();
+            break;
+        case 'VIEW_EMPLOYEES':
+            viewAllEmp();
+            break;
+        case 'UPDATE_EMPLOYEE_ROLE':
+            updateEmpRole();
+            break;
+        case 'QUIT':
+            quit();
+            break;
+        default:
+            break;
+    }
+
+};
+//view dept
+async function viewAllDept() {
+    const departments = await query.findAllDept();
+    console.table(departments);
+    renderPrompts();
+};
+// add dept
+async function addDept() {
+    const { newDeptName } = await prompt([
+        {
+            type: 'input',
+            name: 'newDeptName',
+            message: 'What is the name of Department?'
+        }
+
+    ]);
+    await query.newDept(newDeptName);
+
+    renderPrompts();
+
+}
+
+function quit(){
+    console.log("BYE!")
+    process.exit();
+}
